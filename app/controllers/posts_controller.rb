664d95f483ee24600b12e498631354faa0acf229
mycobble <mycobble@gmail.com>
2011-08-28 21:01:38 -0500

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all(:order => "created_at DESC")
+    respond_to do |format|
+      format.html
+    end
+  end
+
+#def create
+#    @post = Post.create(:message => params[:message])
+ #   respond_to do |format|
+ #     if @post.save
+   #     format.html { redirect_to posts_path }
+     # else
+       # flash[:notice] = "Message failed to save."
+        #format.html { redirect_to posts_path }
+      #end
+    #end
+  #end
+def create  
+  @post = Post.create(:message => params[:message])  
+  respond_to do |format|  
+    if @post.save  
+      format.html { redirect_to posts_path }  
+      format.js  
+    else  
+      flash[:notice] = "Message failed to save."  
+      format.html { redirect_to posts_path }  
+    end  
+  end  
+end  
+
+
+end
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '50303c920028db20e1b83d9195c42064e063c17ec97a4b3061cb5f68b832337b5196d797e6293a296e5705aec307b4155820035f0dcf205d647620f60501a02a'
